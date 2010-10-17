ba45c845701a3be283487113fb848a5f5fc0c57a
Phil Pirj <pirj@mail.ru>
2010-10-17 22:14:50 +0400

    List of uploaded files. Download option.

--- dshelf_example.rb
+++ dshelf_example.rb
@@ -1,10 +1,13 @@
 require 'rubygems'
 require 'sinatra/base'
 require 'haml'
+require 'mime/types'
 
 class DistributedShelfExample < Sinatra::Base
 
-  configure :production do
+  configure do #:production 
+ENV['DISTRIBUTED_SHELF_URL'] = 'http://localhost:8000/storage/3470e95cc331a9f9eea163f5f41e9483'
+
     require 'dshelf'
     DistributedShelf::config = {
       :distributed_path => 'upload',
@@ -13,11 +16,23 @@ class DistributedShelfExample < Sinatra::Base
   end
 
   get '/' do
-    haml <<-hml
+    hml = <<-hml
 %form(action="/upload" method="post" enctype="multipart/form-data")
   %input(type="file" name="file")
   %input(type="submit" value="Upload")
+- files.each do |file|
+  %p
+    %a(href=file)=file
     hml
+    files = Dir.entries('upload')
+    haml hml, :locals => {:files => files}
+  end
+
+  get '/:file' do
+    filename = params[:file]
+    attachment filename
+    content_type MIME::Types.type_for(filename)
+    File.open(File.join('upload', filename)).read
   end
 
   post '/upload' do
@@ -27,5 +42,4 @@ class DistributedShelfExample < Sinatra::Base
     end
     redirect '/'
   end
-
 end
